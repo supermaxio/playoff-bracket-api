7617371836e99eb5f6d492e9898643c8a74ff1e4
supermaxio <maxiomeier64@gmail.com>
2022-12-28 18:12:26 -0600

    New graceful shutdown for gcp

--- cmd/playoff_bracket/main.go
+++ cmd/playoff_bracket/main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/supermaxio/nflplayoffbracket/config"
@@ -27,42 +28,51 @@ func main() {
 	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
 	flag.Parse()
 
-	r := router.Router()
+	router := router.Router()
 
-	srv := &http.Server{
+	server := &http.Server{
 		Addr: fmt.Sprintf("%s:%s", config.GetDomain(), config.GetPort()),
 		// Good practice to set timeouts to avoid Slowloris attacks.
 		WriteTimeout: time.Second * 15,
 		ReadTimeout:  time.Second * 15,
 		IdleTimeout:  time.Second * 60,
-		Handler:      r, // Pass our instance of gorilla/mux in.
+		Handler:      router, // Pass our instance of gorilla/mux in.
 	}
 
 	// Run our server in a goroutine so that it doesn't block.
 	go func() {
-		log.Println("Starting API at: " + srv.Addr)
-		if err := srv.ListenAndServe(); err != nil {
+		log.Println("Starting API at: " + server.Addr)
+		if err := server.ListenAndServe(); err != nil {
 			log.Println(err)
 		}
 	}()
 
-	c := make(chan os.Signal, 1)
+	sigterm := make(chan os.Signal, 1)
 
 	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
 	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
-	signal.Notify(c, os.Interrupt)
-
-	// Block until we receive our signal.
-	<-c
+	signal.Notify(sigterm, os.Interrupt)
+	signal.Notify(sigterm, syscall.SIGTERM)
 
 	// Create a deadline to wait for.
 	ctx, cancel := context.WithTimeout(context.Background(), wait)
 	defer cancel()
 
+	// Wait for the SIGTERM signal or the context to expire
+	select {
+	case <-sigterm:
+		log.Println("Received SIGTERM, shutting down...")
+	case <-ctx.Done():
+		log.Println("Timeout exceeded, shutting down...")
+	}
+
 	// Doesn't block if no connections, but will otherwise wait
 	// until the timeout deadline.
 	// go srv.Shutdown(ctx)
-	srv.Shutdown(ctx)
+	// Shut down the HTTP server gracefully
+	if err := server.Shutdown(ctx); err != nil {
+		log.Println(err)
+	}
 
 	database.MongoDisconnect()
 	log.Println("shutting down")
