7104ef13f1740a3b10b66c72275bf8c697137f65
supermaxio <maxiomeier64@gmail.com>
2023-01-02 17:08:06 -0600

    lowercase validation

--- database/users.go
+++ database/users.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/supermaxio/nflplayoffbracket/constants"
 	"github.com/supermaxio/nflplayoffbracket/types"
@@ -12,6 +13,9 @@ import (
 
 func CreateUser(user types.User) types.User {
 	coll := mongoClient.Database(constants.MONGO_DB_NAME).Collection(constants.USERS_COLLECTION_NAME)
+
+	// validation
+	user.Username = strings.ToLower(user.Username)
 
 	_, err := coll.InsertOne(context.TODO(), user)
 	if err != nil {
@@ -27,6 +31,9 @@ func CreateUser(user types.User) types.User {
 
 func FindUser(username string) (resultUser types.User) {
 	coll := mongoClient.Database(constants.MONGO_DB_NAME).Collection(constants.USERS_COLLECTION_NAME)
+
+	//validation
+	username = strings.ToLower(username)
 
 	err := coll.FindOne(context.TODO(), bson.D{{Key: "username", Value: username}}).Decode(&resultUser)
 	if err != nil {
