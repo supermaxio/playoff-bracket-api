0caa5ced20c44596c42b5bfd19f0bea139d61175
supermaxio <maxiomeier64@gmail.com>
2023-01-04 23:07:15 -0600

    update to brackets model

--- types/bracket.go
+++ types/bracket.go
@@ -2,9 +2,18 @@ package types
 
 type Bracket struct {
 	Username string `json:"username" bson:"username"`
-	WildCardWinners []string `json:"wild_card_winners" bson:"wild_card_winners"`
-	DivisionalRoundWinners []string `json:"divisional_round_winners" bson:"divisional_round_winners"`
-	ConferenceChampions []string `json:"conference_champions" bson:"conference_champions"`
+	AfcWildCard1Winner string `json:"afc_wild_card_1_winner" bson:"afc_wild_card_1_winner"`
+	AfcWildCard2Winner string `json:"afc_wild_card_2_winner" bson:"afc_wild_card_2_winner"`
+	AfcWildCard3Winner string `json:"afc_wild_card_3_winner" bson:"afc_wild_card_3_winner"`
+	AfcDivisionalRound1Winner string `json:"afc_divisional_round_1_winner" bson:"afc_divisional_round_1_winner"`
+	AfcDivisionalRound2Winner string `json:"afc_divisional_round_2_winner" bson:"afc_divisional_round_2_winner"`
+	AfcConferenceChampion string `json:"afc_conference_champion" bson:"afc_conference_champion"`
+	NfcWildCard1Winner string `json:"nfc_wild_card_1_winner" bson:"nfc_wild_card_1_winner"`
+	NfcWildCard2Winner string `json:"nfc_wild_card_2_winner" bson:"nfc_wild_card_2_winner"`
+	NfcWildCard3Winner string `json:"nfc_wild_card_3_winner" bson:"nfc_wild_card_3_winner"`
+	NfcDivisionalRound1Winner string `json:"nfc_divisional_round_1_winner" bson:"nfc_divisional_round_1_winner"`
+	NfcDivisionalRound2Winner string `json:"nfc_divisional_round_2_winner" bson:"nfc_divisional_round_2_winner"`
+	NfcConferenceChampion string `json:"nfc_conference_champion" bson:"nfc_conference_champion"`
 	SuperBowlChampion string `json:"super_bowl_champion" bson:"super_bowl_champion"`
 	FinalScoreSum int `json:"final_score_sum" bson:"final_score_sum"`
 }
